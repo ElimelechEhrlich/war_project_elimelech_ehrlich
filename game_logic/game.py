0cfee70a3d4b6bd003e9bf763d8d712118392907
Elimelech Ehrlich <ee525566@gmail.com>
2025-10-23 09:27:22 +0300

    create folders and files of tha test

---
+++ game_logic/game.py
@@ -0,0 +1,6 @@
+
+create_player(name:str) -> dict
+
+init_game()->dict
+
+play_round(p1:dict,p2:dict)
---
+++ utils/deck.py
@@ -0,0 +1,4 @@
+a. create_card(rank:str,suite:str) -> dict
+b. compare_cards(p1_card:dict, p2_card:dict) -> str
+c. create_deck() -> list[dict]
+d. shuffle(deck:list[dict]) -> list[dict]
