74c594d7d74dbdf4fa20ee15cd0dc64a539f66d8
ElimelechEhrlich <ee525566@gmail.com>
2025-10-23 20:54:20 +0300

    Update game initialization and round play logic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from game_logic.game import init_game play_round
 
 if __name__=="__main__":
-    init_game()
-    play_round()
+    corrunt_game = init_game()
+    play_round(corrunt_game['player_1'],corrunt_game['player_2'])
+    
