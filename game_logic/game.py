9c9a349edf993cc5f58f4177d3c15fdd8b8870f7
ElimelechEhrlich <ee525566@gmail.com>
2025-10-23 23:43:40 +0300

    Fix card addition and player round logic

--- game_logic/game.py
+++ game_logic/game.py
@@ -12,10 +12,8 @@ def init_game():
     player2 = create_player()
     hend_of_player1 = player1['hand']
     hend_of_player2 = player2['hand']
-    cards_for_player1 = Current_deck[0:26]
-    cards_for_player2 = Current_deck[26:-1]
-    add_cards_player1 = hend_of_player1.append[cards_for_player1]
-    add_cards_player2 = hend_of_player2.append[cards_for_player1]
+    add_cards_player1 = hend_of_player1.extend(Current_deck[0:26])
+    add_cards_player2 = hend_of_player2.extend(Current_deck[26:-1])
     return  {"deck": Current_deck ,"player_1": add_cards_player1,"player_2": add_cards_player2}
 
 def play_round(player_1: dict, player_2: dict):
@@ -24,12 +22,21 @@ def play_round(player_1: dict, player_2: dict):
     won_pile_of_player1 = player_1['won_pile']
     won_pile_of_player2 = player_2['won_pile']
     Compared = deck.compare_cards(hend_of_player1[-1], hend_of_player2[-1])
-    if  Compared == 'p1':
-        print ('P1 Winner!!!')
-        won_pile_of_player1.append[hend_of_player1.pop[-1]]
-    elif  Compared == 'p2':
-        print ('P2 Winner!!!')
-        won_pile_of_player2.append[hend_of_player2.pop[-1]]
-    print ('by hend p1: ',len(hend_of_player1))
-    print ('by hend p1: ',len(hend_of_player2))
+    while (len(hend_of_player1) > 0) and (len(hend_of_player2) > 0):
+        if  Compared == 'p1':
+            print ('P1 Winner!!!')
+            won_pile_of_player1.append(hend_of_player1.pop(hend_of_player1[-1])
+            won_pile_of_player1.append(hend_of_player2.pop(hend_of_player2[-1]))
+        elif  Compared == 'p2':
+            print ('P2 Winner!!!')
+            won_pile_of_player2.append(hend_of_player2.pop(hend_of_player2[-1]))
+            won_pile_of_player2.append(hend_of_player1.pop(hend_of_player1[-1]))
+        else:
+            pass
+        print ('by hend p1:', len(hend_of_player1))
+        print ('by hend p2:', len(hend_of_player2))
+        print ('by won_pile p1:', len(hend_of_player1))
+        print ('by won_pile p2:', len(hend_of_player2))
+        
+        
 
