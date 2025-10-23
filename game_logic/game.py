3b1e3ec9c15f20bb04771177b24dff1a707bf2bb
Elimelech Ehrlich <ee525566@gmail.com>
2025-10-23 13:21:13 +0300

    init

--- game_logic/game.py
+++ game_logic/game.py
@@ -4,7 +4,9 @@ def create_player(name:str):
     return {"name":"","hand":[],"won_pile":[]}
 
 def init_game():
-    
+
+    deck.shuffle(deck.create_deck())
+    deck.play_round(deck.create_player(__name__), deck.create_player(__name__))
 
     return  {"deck": "<the deck dict you created, shuffled>","player_1": "<human player dict you created>","player_2": "<AI player dict you created>"}
 
