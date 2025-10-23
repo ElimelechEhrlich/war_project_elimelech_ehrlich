afe11b38256a1c90220b670bd69e59059932eb79
ElimelechEhrlich <ee525566@gmail.com>
2025-10-23 23:21:17 +0000

    fixed

--- game_logic/game.py
+++ game_logic/game.py
@@ -1,3 +1,4 @@
+
 from utils import deck
 
 def create_player(name:str='AI'):
@@ -25,18 +26,18 @@ def play_round(player_1: dict, player_2: dict):
     while (len(hend_of_player1) > 0) and (len(hend_of_player2) > 0):
         if  Compared == 'p1':
             print ('P1 Winner!!!')
-            won_pile_of_player1.append(hend_of_player1.pop(hend_of_player1[-1])
-            won_pile_of_player1.append(hend_of_player2.pop(hend_of_player2[-1]))
+            won_pile_of_player1.extenb(hend_of_player1.pop(-1))
+            won_pile_of_player1.extend(hend_of_player2.pop(-1))
         elif  Compared == 'p2':
             print ('P2 Winner!!!')
-            won_pile_of_player2.append(hend_of_player2.pop(hend_of_player2[-1]))
-            won_pile_of_player2.append(hend_of_player1.pop(hend_of_player1[-1]))
+            won_pile_of_player2.extend(hend_of_player2.pop(hend_of_player2[-1]))
+            won_pile_of_player2.extend(hend_of_player1.pop(hend_of_player1[-1]))
         else:
             pass
         print ('by hend p1:', len(hend_of_player1))
         print ('by hend p2:', len(hend_of_player2))
         print ('by won_pile p1:', len(hend_of_player1))
         print ('by won_pile p2:', len(hend_of_player2))
-        
+    return
         
 
--- utils/deck.py
+++ utils/deck.py
@@ -1,30 +1,28 @@
 import random
 
 def create_card(rank:str,suite:str):
-    ranks = {'2' : '2' , '3' : '3' , '4' : '4' , '5' : '5' , '6' : '6' , '7' : '7' , '8' : '8' , '9' : '9' , '10' : '10' , '11' : 'J' , '12' : 'Q' , '13' : 'K' , '14' : 'A'}
+    ranks = {'2' : '2' , '3' : '3' , '4' : '4' , '5' : '5' , '6' : '6' , '7' : '7' , '8' : '8' , '9' : '9' , '10' : '10' ,  'J' : '11' , 'Q' : '12' , 'K' : '13' , 'A' : '14'}
     suites = ['H', 'C', 'D', 'S']
-    for letter in ranks:
-        if  letter == rank:
-            for i in suites:
-                if i == suite:
-                    return ({'rank': rank, 'suite': suite, 'value': letter})
-            return ('Incorrect suite input')
-    return ('Incorrect rank input')
+    if rank in ranks.keys() and suite in suites:
+        return ({'rank': rank, 'suite': suite, 'value': ranks[rank]})
+    else:
+        return ('Incorrect suite or rank input')
+    
 
 def compare_cards(p1_card:dict, p2_card:dict):
     try:
-        if p1_card['value'] > p2_card['value']:
+        if p1_card['rank'] > p2_card['rank']:
             return ('p1')
-        elif p2_card['value'] > p1_card['value']:
+        elif p2_card['rank'] > p1_card['rank']:
             return ('p2')
-        elif p2_card['value'] == p1_card['value']:
+        elif p2_card['rank'] == p1_card['rank']:
             return ('WAR')
     except:
         print (f'"value" key is does not exist in Or its value is not of type number at least one of the dictionaries')
     return
                                       
 def create_deck(): 
-    ranks = {'2' : '2' , '3' : '3' , '4' : '4' , '5' : '5' , '6' : '6' , '7' : '7' , '8' : '8' , '9' : '9' , '10' : '10' , '11' : 'J' , '12' : 'Q' , '13' : 'K' , '14' : 'A'}
+    ranks = {'2' : '2' , '3' : '3' , '4' : '4' , '5' : '5' , '6' : '6' , '7' : '7' , '8' : '8' , '9' : '9' , '10' : '10' , 'J' : '11' , 'Q' : '12' , 'K' : '13' , 'A' : '12'}
     suites = ['H', 'C', 'D', 'S']
     deck = []
     for rank in ranks:
@@ -36,9 +34,8 @@ def shuffle(deck:list[dict]):
     for run in range(1000):
         index1 = random.randrange(len(deck))
         index2 = random.randrange(len(deck))
-        if index1 == index2:
-            index1 = random.randrange(len(deck))
-        deck[index1], deck[index2] = deck[index2], deck[index1]
+        if index1 != index2:
+            deck[index1], deck[index2] = deck[index2], deck[index1]
     return deck
 
 
