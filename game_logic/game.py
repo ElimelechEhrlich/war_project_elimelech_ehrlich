2d4b00d1a672e7688b1f3e3163f255f043db9949
ElimelechEhrlich <ee525566@gmail.com>
2025-10-23 22:09:10 +0300

    Fix player hand variable names and append method
    Corrected variable names from 'hend' to 'hand' for player hands and fixed append method usage.

--- game_logic/game.py
+++ game_logic/game.py
@@ -10,15 +10,15 @@ def init_game():
     Current_deck = [deck.shuffle(deck.create_deck())]
     player1 = create_player('eli')
     playet2 = create_player()
-    hend_of_player1 = player1['hend']
-    hend_of_player2 = player2['hend']
+    hend_of_player1 = player1['hand']
+    hend_of_player2 = player2['hand']
     cards_player1 = hend_of_player1.append[Current_deck[0:26]]
     cards_player2 = hend_of_player2.append[Current_deck[26:-1]]
     return  {"deck": Current_deck ,"player_1": cards_player1,"player_2": cards_player2}
 
 def play_round(player_1: dict, player_2: dict):
-    hend_of_player1 = player_1['hend']
-    hend_of_player2 = player_2['hend']
+    hend_of_player1 = player_1['hand']
+    hend_of_player2 = player_2['hand']
     won_pile_of_player1 = player_1['won_pile']
     won_pile_of_player2 = player_2['won_pile']
     Compared = deck.compare_cards(hend_of_player1[-1], hend_of_player2[-1])
