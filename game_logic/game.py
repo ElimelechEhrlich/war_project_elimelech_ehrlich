68804bb5e605dc2dcfad3f3e76917d654b405c96
ElimelechEhrlich <ee525566@gmail.com>
2025-10-23 18:18:41 +0000

    fixed

--- game_logic/game.py
+++ game_logic/game.py
@@ -1,4 +1,4 @@
-import utils.deck
+from utils import deck
 
 def create_player(name:str='AI'):
     hand = []
@@ -13,12 +13,12 @@ def init_game():
     return  {"deck": Current_deck ,"player_1": player1,"player_2": player2}
 
 def play_round(player_1: dict, player_2: dict):
-    hend_of_player1 = player_1['hend']]
-    hend_of_player2 = player_2['hend']]
+    hend_of_player1 = player_1['hend']
+    hend_of_player2 = player_2['hend']
     Compared = deck.compare_cards(hend_of_player1[-1], hend_of_player2[-1])
     if  Compared == 'p1':
         print ('P1 Winner!!!')
-        Corrent_game['player_1'['won_pile']].append[hend_of_player1.pop[-1]]
+        player_1['won_pile'].append[hend_of_player1.pop[-1]]
     elif  Compared == 'p2':
         print ('P2 Winner!!!')
         player_2['won_pile'].append[hend_of_player2.pop[-1]]
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from game_logic.game import init_game play_round
+from game_logic.game import init_game, play_round
 
 if __name__=="__main__":
     corrunt_game = init_game()
